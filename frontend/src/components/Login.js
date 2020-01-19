f8a015fd5140e4daeae2d49c74cbdce2941ca4a6
Rick Leinecker <rick@jsventures.com>
2020-01-19 11:33:21 -0800

    third commit

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -19,7 +19,8 @@ function Login()
 
         try
         {    
-            const response = await fetch('http://localhost:5000/api/login',
+//            const response = await fetch('http://localhost:5000/api/login',
+            const response = await fetch('http://localhost/api/login',
                 {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
 
             var res = JSON.parse(await response.text());
